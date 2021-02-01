05e036962ed46b130a30637223b361fb82201a52
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-02-01 14:55:20 +0100

    class constructors

---
+++ Learn_OOPHP/chapter2/Comment.php
@@ -0,0 +1,11 @@
+<?php
+class Comment 
+{
+    public $text;
+    public $userId;
+
+    public function __construct($text, $userId) {
+        $this->text = $text;
+        $this->userId = $userId;
+    }
+}
